c46fc56c170a529cdc0f2bb69f14e41e328e1665
PatrikasDapsys <patrikas.dapsys@gmail.com>
2023-09-29 17:00:44 +0300

    search routing

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -12,7 +12,9 @@
         class="absolute bg-weather-secondary text-white w-full shadow-md py-2 px-1 top-[66px]"
         v-if="mapboxSearchResults"
       >
-        <p class="py-2" v-if="searchError">Sorry, something went wrong, please try again.</p>
+        <p class="py-2" v-if="searchError">
+          Sorry, something went wrong, please try again.
+        </p>
         <p class="py-2" v-if="!searchError && mapboxSearchResults.length === 0">
           No results, try a different term.
         </p>
@@ -21,6 +23,7 @@
             v-for="searchResult in mapboxSearchResults"
             :key="searchResult.id"
             class="py-2 cursor-pointer"
+            @click="previewCitys(searchResult)"
           >
             {{ searchResult.place_name }}
           </li>
@@ -33,13 +36,29 @@
 <script setup>
 import { ref } from "vue";
 import axios from "axios";
+import { useRouter } from "vue-router";
 
+const router = useRouter();
 const apiKey =
   "pk.eyJ1IjoicGF0cmlrYXNkYXBzeXMiLCJhIjoiY2xuMzhvOTd2MGdhYTJpcjNldnJsaWxtMSJ9.hDzpqbLtDvpDxFye8pasfw";
 const searchQuery = ref("");
 const queryTimeout = ref(null);
 const mapboxSearchResults = ref(null);
 const searchError = ref(null);
+
+const previewCitys = (searchResult) => {
+  console.log(searchResult);
+  const [city, state] = searchResult.place_name.split(",");
+  router.push({
+    name: "cityView",
+    params: { state: state.replaceAll(" ", ""), city: city },
+    query: {
+      lat: searchResult.geometry.coordinates[1],
+      lng: searchResult.geometry.coordinates[0],
+      preview: true,
+    },
+  });
+};
 
 const getSearchResults = () => {
   clearTimeout(queryTimeout.value);
